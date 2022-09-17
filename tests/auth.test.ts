7b61b5601c816dead5d3d5f83d3258291ab59d47
pgeovany <pgeovany@ic.ufrj.br>
2022-09-17 20:10:41 -0300

    test: add tests for sign-in route

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -2,6 +2,7 @@ import prisma from '../src/database/database';
 import supertest from 'supertest';
 import app from '../src/app';
 import userFactory from './factories/userFactory';
+import encrypt from '../src/utils/encrypt';
 
 beforeEach(async () => {
   await prisma.$executeRaw`TRUNCATE TABLE users`;
@@ -40,5 +41,37 @@ describe('POST /sign-up', () => {
       .send({ ...user, confirmPassword: user.password });
 
     expect(duplicateUser.status).toEqual(409);
+  });
+});
+
+describe('POST /sign-in', () => {
+  it('Should return status 422 given invalid params', async () => {
+    const result = await supertest(app).post('/sign-in').send({});
+
+    expect(result.status).toEqual(422);
+  });
+
+  it('Should return status 401 given an invalid user', async () => {
+    const user = userFactory();
+
+    const result = await supertest(app).post('/sign-in').send(user);
+
+    expect(result.status).toEqual(401);
+  });
+
+  it('Should return status 200 and a token given a valid user', async () => {
+    const user = userFactory();
+
+    const data = {
+      email: user.email,
+      password: await encrypt(user.password),
+    };
+
+    await prisma.users.create({ data });
+
+    const result = await supertest(app).post('/sign-in').send(user);
+
+    expect(result.body.token).not.toBeUndefined();
+    expect(result.status).toEqual(200);
   });
 });
